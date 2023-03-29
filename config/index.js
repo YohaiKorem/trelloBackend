b5074faaae0c327b2ece3ff42d2c945c9f5cace0
YohaiKorem <Yohaycasoy@gmail.com>
2023-03-29 16:57:18 +0300

    added environment vars

--- config/index.js
+++ config/index.js
@@ -3,7 +3,7 @@ var config
 if (process.env.NODE_ENV === 'production') {
   config = require('./prod')
 } else {
-  config = require('./prod')
+  config = require('./dev')
   // config = require('./dev')
 }
 
